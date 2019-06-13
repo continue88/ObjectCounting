4c79b84bc305cc7701d361dd58cacedabbbef73d
zhouqi <zhouqi@dianhun.cn>
2019-06-13 16:18:33 +0800

    更新运行代码整理

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import sys
 import util
 import numpy as np
 import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
 
 epochs = 1000
 epoch_save = 10
@@ -19,62 +15,15 @@ weight_path = 'modle.h5'
 #util.extract_video('data/validate/*.mp4', 5, input_shape)
 
 print('building modle...')
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
-
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
+model = util.build_modle(input_shape, num_classes)
 
 if os.path.exists(weight_path):
     print('load model weight...')
     model.load_weights(weight_path)
 
 if '--train' in sys.argv:
-    print('load the trainning data...')
-    for epoch in range(epochs):
-        data_set = util.load_dataset('data/image/*.*', load_num, input_shape)
-        x_train = np.array(data_set[0]).astype(np.float32) / 255.0
-        y_train = keras.utils.to_categorical(data_set[1], num_classes)
-
-        train_num = x_train.shape[0]
-        indices = np.arange(train_num)
-
-        np.random.shuffle(indices)
-        n_batches = int((train_num + batch_size - 1) / batch_size)
-        for batch in range(n_batches):
-            start = batch * batch_size
-            end = (batch + 1) * batch_size
-            if end > train_num:
-                end = train_num
-            x_batch = x_train[start:end]
-            y_batch = y_train[start:end]
-            loss = model.train_on_batch(x_batch, y_batch)
-            print("[Epoch %d/%d] [Batch %d/%d] [D loss %f, acc: %3f%%]" % (epoch, epochs, batch, n_batches, loss[0], loss[1] * 100))
-        if epoch % epoch_save == 0:
-            model.save_weights(weight_path)
-
-print('load the test data...')
-test_set = util.load_dataset('data/validate/*.*', load_num, input_shape, False)
-x_test = np.array(test_set[0]).astype(np.float32) / 255.0
-y_test = keras.utils.to_categorical(test_set[1], num_classes)
+    print('trainning data...')
+    util.train(model, 'data/image/*.*', load_num, input_shape, num_classes, epochs, batch_size, epoch_save, weight_path)
 
 print('predict data...')
-result = model.predict(x_test)
-for i in range(len(result)):
-    value = np.argmax(result[i])
-    print('predict %d, actual %d' % (value, test_set[1][i]))
-
-print('test the trainning data...')
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy: %f', score[1])
+util.predict(model, 'data/validate/*.*', load_num, input_shape, num_classes)
--- util.py
+++ util.py
@@ -1,7 +1,68 @@
-
+import os
 import cv2
 import glob
 import numpy as np
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+
+def build_modle(input_shape, num_classes):
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                    activation='relu',
+                    input_shape=input_shape))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                optimizer=keras.optimizers.Adadelta(),
+                metrics=['accuracy'])
+    return model
+
+def train(model, data_path, load_num, input_shape, num_classes, epochs, batch_size, epoch_save, weight_path):
+    for epoch in range(epochs):
+        data_set = load_dataset(data_path, load_num, input_shape)
+        x_train = np.array(data_set[0]).astype(np.float32) / 255.0
+        y_train = keras.utils.to_categorical(data_set[1], num_classes)
+
+        train_num = x_train.shape[0]
+        indices = np.arange(train_num)
+
+        np.random.shuffle(indices)
+        n_batches = int((train_num + batch_size - 1) / batch_size)
+        for batch in range(n_batches):
+            start = batch * batch_size
+            end = (batch + 1) * batch_size
+            if end > train_num:
+                end = train_num
+            x_batch = x_train[start:end]
+            y_batch = y_train[start:end]
+            loss = model.train_on_batch(x_batch, y_batch)
+            print("[Epoch %d/%d] [Batch %d/%d] [D loss %f, acc: %3f%%]" % (epoch, epochs, batch, n_batches, loss[0], loss[1] * 100))
+        if epoch % epoch_save == 0:
+            model.save_weights(weight_path)
+
+def predict(model, validate_path, load_num, input_shape, num_classes):
+    test_set = load_dataset(validate_path, load_num, input_shape, False)
+    x_test = np.array(test_set[0]).astype(np.float32) / 255.0
+    y_test = keras.utils.to_categorical(test_set[1], num_classes)
+
+    print('predict data...')
+    result = model.predict(x_test)
+    for i in range(len(result)):
+        value = np.argmax(result[i])
+        print('predict %d, actual %d' % (value, test_set[1][i]))
+
+    print('test the trainning data...')
+    score = model.evaluate(x_test, y_test, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy: %f', score[1])
 
 def extract_video(path, skip, input_shape):
     file_list = glob.glob(path)
@@ -35,18 +96,16 @@ def load_dataset(path, load_num, input_shape, train=True):
     height, width, channels = input_shape
     img_list = []
     label_list = []
-    rotate_range = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330]
     for file in file_list:
-        begin = file.rfind('\\') + 1
-        end = file.rfind(' ')
+        file_name = os.path.basename(file)
+        end = file_name.rfind(' ')
         if end < 0:
-            end = file.rfind('.')
-        label = int(file[begin:end])
+            end = file_name.rfind('.')
+        label = int(file_name[:end])
         img = cv2.imread(file)
         img = cv2.resize(img, (width, height))
         if train:
-            rot = np.random.randint(0, len(rotate_range))
-            angle = rotate_range[rot]
+            angle = np.random.randint(0, 360)
             img = rotate_image(img, angle)
         img_list.append(img)
         label_list.append(label)
